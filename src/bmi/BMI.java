153f2469ccbf46b5b50c59b0b26935bfc20b00ee
Informaster <arthur.pabst@t-online.de>
2014-12-14 12:36:14 +0100

    1.

---
+++ src/bmi/BMI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bmi;
+
+/**
+ *
+ * @author Arthur
+ */
+public class BMI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           
+        Startbildschirm start=new Startbildschirm();
+        start.setVisible(true);
+        start.setSize(800,600);
+    }
+}
